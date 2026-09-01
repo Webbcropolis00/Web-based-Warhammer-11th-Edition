809140c9e7f63cc87c8ce7cf0b6d962f518d5bca
hwebb01 <hwebb01@BD14409.student.ad.uafortsmith.edu>
2026-09-01 14:36:20 -0500

    Alpha 0.0.1

--- WebBasedWarhammer11th/src/model/Model.java
+++ WebBasedWarhammer11th/src/model/Model.java
@@ -10,6 +10,22 @@ public class Model {
 	int oc;
 	double x;
 	double y;
+	double r;
 	
+	public Model() {
+		m = 6.00;
+		t = 4; 
+		sv = 3;
+		insv = -1;
+		w = 2;
+		ld = 6;
+		oc = 2;
+		x = -1;
+		y = -1;
+		r = 1.26;
+	}
 	
+	public void Model(String army, String unit) {
+		
+	}
 }
